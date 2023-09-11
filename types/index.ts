3463e43c6c353c2a53b23dbdd560bdd7404e10d4
shahmeerbaig2 <57468685+shahmeerbaig2@users.noreply.github.com>
2023-09-11 16:25:50 +0500

    fourth commit

--- types/index.ts
+++ types/index.ts
@@ -9,3 +9,18 @@ export interface SearchManufacturerProps{
     manufacturer:String,
     setManufacturer:(manufacturer:string)=>void;
 }
+
+export interface CarProps{
+    city_mpg:number;
+class:string;
+combination_mpnumber:number;
+cylindenumber:number;
+displacementnumber:number;
+drive:string;
+fuel_type:string;
+highway_mpnumber:number;
+make:string;
+model:string;
+transmission:string;
+year:number;
+}
